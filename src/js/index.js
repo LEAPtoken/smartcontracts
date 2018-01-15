5dfcad09aa2237f0fd524fe09cccf06de87baaf4
sevenflash <sevenflash@yandex.ru>
2018-01-15 22:42:52 +0300

    mainnet

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,4 @@ import ReactDOM from 'react-dom';
 
 import Dashboard from './Dashboard';
 
-ReactDOM.render(<Dashboard proxy='https://ropsten.infura.io/jPkVat66IVKkmtAsy0DJ' />, document.getElementById('content'));
+ReactDOM.render(<Dashboard proxy='https://mainnet.infura.io/jPkVat66IVKkmtAsy0DJ' />, document.getElementById('content'));
